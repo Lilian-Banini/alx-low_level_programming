57a36875fe2fbb8d11d33ca6948073159d59e046
lilian <lilianbanini319@gmail.com>
2022-12-08 19:43:05 -0800

    c file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - print a line of code using puts
  *
- * Return: always 0
+ * Return: always 0 (Success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+        return (0)
 }
