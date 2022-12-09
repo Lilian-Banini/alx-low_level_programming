d47ce1415fa92cf81d5012b5f49f427d134bda78
lilian <lilianbanini319@gmail.com>
2022-12-08 20:39:52 -0800

    getting there

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a c program usingprintf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return	(0);
+}
