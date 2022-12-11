6e5ab9eec9bbc73fb1ac8efa289ffdd7346047be
Isaac Wilson <iswilson777@gmail.com>
2022-12-11 12:39:00 +0000

    Added 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0 (success)
+*/
+int main(void)
+{	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+		putchar('\n');
+	}
+	return (0);
+}
