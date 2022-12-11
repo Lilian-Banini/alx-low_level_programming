eeef28d1bfb21be384cf719ff03a3e39d4995fee
Isaac Wilson <iswilson777@gmail.com>
2022-12-11 13:10:16 +0000

    Added 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - prints all alphabets except e and q
+*
+*Return: 0 (success)
+*/
+int main(void)
+{	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'z')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
