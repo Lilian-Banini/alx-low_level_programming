284a0f14bd72fe7789f479593eb3073ff1c0456a
Isaac Wilson <iswilson777@gmail.com>
2022-12-13 21:44:36 +0000

    Modified 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,21 +2,21 @@
 /**
  *print_sign - prints sign of a number
  *
- * @c: - number to be checked sign
+ * @n: - number to be checked sign
  *
  * Return: 1 (positive) , 0 (zero), -1 (negative)
  */
 int print_sign(int n)
 {
 		if (n > 0)
-		{
+
 			_putchar('+');
 			return (1);
 		}
 		else if (n < 0)
 		{
 			_putchar('-');
-			return(-1);
+			return (-1);
 		}
 		else
 		{
