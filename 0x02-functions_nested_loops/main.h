43607514d937a17fa4f553af87175bd8d2e8835f
lilian <lilianbanini319@gmail.com>
2022-12-13 15:18:34 -0800

    nest loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef   _MAIN_H_
+#define  _MAIN_H_
+
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+
+#endif
