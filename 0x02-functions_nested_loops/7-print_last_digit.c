d9a92d82a10558156b89e91b9b8da91713d70837
Isaac Wilson <iswilson777@gmail.com>
2022-12-14 00:18:24 +0000

    fixed errors in file name

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit - returns last digit of n
+ *
+ *@n: - number to be operated on
+ *
+ *Return: last_digit of n (success)
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
