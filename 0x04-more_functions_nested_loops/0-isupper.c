fb6acfd9b4c24286a9be3f1e3e327a0ac4f71860
lilian <lilianbanini319@gmail.com>
2022-12-16 15:42:57 -0800

    task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,24 @@
-#include "main.h"
+#include "main.h"
 /**
-*_isupper - checks whether a character
-*passed to it is lowercase or uppercase
-*character
+*_isupper - checks whether a character
+*passed to it is lowercase or uppercase
+*character.
 *
-*@c : character to be checked
+*@c : character to be checked
 *
-*Return: 1 (uppercase), 0 (lowercase)
+*Return: 1 (uppercase), 0 (lowercase)
 */
-int _isupper(int c)
+int _isupper(int c)
 {
 
-        int alphabet;
+	int alphabet;
 
-        for (alphabet = 97; alphabet <= 122;  alphabet++)
-        {
-                if (c == alphabet)
-                {
-                        return (0);
-                }
-        }
-        return (1);
+	for (alphabet = 97; alphabet <= 122; alphabet++)
+	{
+		if (c == alphabet)
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
