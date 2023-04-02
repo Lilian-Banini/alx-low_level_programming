b374b817dff0d434926d28c02adcab1ed1658393
Lilian Banini Quarshie <lilianbanini319@gmail.com>
2023-04-02 14:16:26 -0700

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 /*
  * File: lists.h
- * Auth: Brennan D Baraban
+ * Auth: Lilian Banini
  * Desc: Header file containing prototypes and definitions for all functions
  *       and types written in the 0x11-singly_linked_lists directory.
  */
